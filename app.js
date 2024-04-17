ef639ed8d8450fd77e4c21f02a8948b4c57d57f3
ehuntober <ehuntercrypto@gmail.com>
2024-04-17 09:38:14 +0100

    application init --wip

--- app.js
+++ app.js
@@ -13,3 +13,34 @@ const {initializeRealtime} = require("./utils/realtime");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+
+// connect to MongoDB
+const dbConnect = () =>{
+    try{
+        const conn = mongoose.connect(process.env.MONGODB_URL)
+        console.log("Database connected successfully")
+    } catch(error){
+        console.log('Database error')
+    }
+}
+
+dbConnect()
+
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
+
+// Initialize real-time updates
+initializeRealtime(app);
+
+// API routes
+app.use('/api/auth', authRoutes);
+app.use('/api/forums', verifyAPIKey, forumRoutes);
+app.use('/api/posts', verifyAPIKey, postRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
