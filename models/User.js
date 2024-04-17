7ab03f644b5a6e47fb15ff37332604b77d470197
ehuntober <ehuntercrypto@gmail.com>
2024-04-17 13:49:48 +0100

    new file added

--- models/User.js
+++ models/User.js
@@ -1,3 +1,37 @@
+// // models/User.js
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcrypt');
+
+// const userSchema = new mongoose.Schema({
+//   username: { type: String, required: true, unique: true },
+//   password: { type: String, required: true },
+//   role: { type: String, enum: ['user', 'moderator', 'admin'], default: 'user' },
+//   createdAt: { type: Date, default: Date.now },
+//   updatedAt: { type: Date, default: Date.now },
+// });
+
+// userSchema.pre('save', function(next) {
+//   this.updatedAt = new Date();
+//   next();
+// });
+
+// userSchema.statics.findByCredentials = async function(username, password) {
+//   const user = await this.findOne({ username });
+//   if (!user) {
+//     throw new Error('Invalid username or password');
+//   }
+
+//   const isMatch = await bcrypt.compare(password, user.password);
+//   if (!isMatch) {
+//     throw new Error('Invalid username or password');
+//   }
+
+//   return user;
+// };
+
+// module.exports = mongoose.model('User', userSchema);
+
+
 // models/User.js
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
@@ -10,7 +44,11 @@ const userSchema = new mongoose.Schema({
   updatedAt: { type: Date, default: Date.now },
 });
 
-userSchema.pre('save', function(next) {
+userSchema.pre('save', async function(next) {
+  if (this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
   this.updatedAt = new Date();
   next();
 });
