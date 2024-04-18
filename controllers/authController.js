49be03769e85241416aad40bd438c5f4b233841e
ehuntober <ehuntercrypto@gmail.com>
2024-04-18 12:49:14 +0100

    new file updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,34 @@
+// // controllers/authController.js
+// const authService = require('../services/authService');
+
+// exports.register = async (req, res) => {
+//   try {
+//     const { username, password } = req.body;
+//     const user = await authService.register(username, password);
+//     res.status(201).json(user);
+//   } catch (err) {
+//     res.status(400).json({ error: err.message });
+//   }
+// };
+
+// exports.login = async (req, res) => {
+//   try {
+//     const { username, password } = req.body;
+//     const { user, token, apiKey } = await authService.login(username, password);
+//     res.json({ user, token, apiKey });
+//   } catch (err) {
+//     res.status(401).json({ error: err.message });
+//   }
+// };
+
 // controllers/authController.js
 const authService = require('../services/authService');
 
 exports.register = async (req, res) => {
   try {
     const { username, password } = req.body;
-    const user = await authService.register(username, password);
-    res.status(201).json(user);
+    const { user, token, apiKey } = await authService.register(username, password);
+    res.status(201).json({ user, token, apiKey });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
@@ -18,5 +41,14 @@ exports.login = async (req, res) => {
     res.json({ user, token, apiKey });
   } catch (err) {
     res.status(401).json({ error: err.message });
+  }
+};
+
+exports.logout = async (req, res) => {
+  try {
+    await authService.logout(req.user.id);
+    res.json({ message: 'Logged out successfully' });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
   }
 };
--- services/authService.js
+++ services/authService.js
@@ -1,3 +1,56 @@
+// // services/authService.js
+// const jwt = require('jsonwebtoken');
+// const User = require('../models/User');
+// const { generateAPIKey } = require('../utils/apiKeyGenerator');
+
+// const JWT_SECRET = 'your_secret_key';
+
+// exports.register = async (username, password) => {
+//   const user = await User.create({ username, password });
+//   return user;
+// };
+
+// exports.login = async (username, password) => {
+//   const user = await User.findByCredentials(username, password);
+//   const token = generateJWT(user._id);
+//   const apiKey = user.apiKey;
+//   return { user, token, apiKey };
+// };
+
+// exports.findUserById = async (userId) => {
+//   return await User.findById(userId);
+// };
+
+// exports.verifyToken = async (token) => {
+//   try {
+//     const decoded = jwt.verify(token, JWT_SECRET);
+//     const user = await exports.findUserById(decoded.userId);
+//     if (!user) {
+//       throw new Error('Invalid token');
+//     }
+//     return user;
+//   } catch (err) {
+//     throw new Error('Invalid token');
+//   }
+// };
+
+// exports.authorizeUser = async (userId, requiredRole) => {
+//   const user = await exports.findUserById(userId);
+//   if (!user) {
+//     throw new Error('User not found');
+//   }
+//   if (user.role !== requiredRole) {
+//     throw new Error('Unauthorized');
+//   }
+//   return user;
+// };
+
+// const generateJWT = (userId) => {
+//   return jwt.sign({ userId }, JWT_SECRET, { expiresIn: '1h' });
+// };
+
+
+
 // services/authService.js
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
@@ -7,7 +60,9 @@ const JWT_SECRET = 'your_secret_key';
 
 exports.register = async (username, password) => {
   const user = await User.create({ username, password });
-  return user;
+  const token = generateJWT(user._id);
+  const apiKey = user.apiKey;
+  return { user, token, apiKey };
 };
 
 exports.login = async (username, password) => {
@@ -15,6 +70,10 @@ exports.login = async (username, password) => {
   const token = generateJWT(user._id);
   const apiKey = user.apiKey;
   return { user, token, apiKey };
+};
+
+exports.logout = async (userId) => {
+  // Implement logout functionality, e.g., invalidate the JWT token
 };
 
 exports.findUserById = async (userId) => {
