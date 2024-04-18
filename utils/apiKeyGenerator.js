7412592b56849893ba61881837bd18fd1390757c
ehuntober <ehuntercrypto@gmail.com>
2024-04-18 11:18:52 +0100

    new user added

--- utils/apiKeyGenerator.js
+++ utils/apiKeyGenerator.js
@@ -1,11 +1,24 @@
+// // utils/apiKeyGenerator.js
+// const jwt = require('jsonwebtoken');
+
+// const generateAPIKey = (userId) => {
+//   const secret = 'your_secret_key';
+//   const payload = { userId };
+//   const options = { expiresIn: '7d' };
+//   return jwt.sign(payload, secret, options);
+// };
+
+// module.exports = {
+//   generateAPIKey,
+// };
+
+
 // utils/apiKeyGenerator.js
-const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 
 const generateAPIKey = (userId) => {
-  const secret = 'your_secret_key';
-  const payload = { userId };
-  const options = { expiresIn: '7d' };
-  return jwt.sign(payload, secret, options);
+  const apiKey = crypto.randomBytes(20).toString('hex');
+  return apiKey;
 };
 
 module.exports = {
