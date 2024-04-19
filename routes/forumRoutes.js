c8cdeefa4847d6d045d9011351d117fc14858c36
ehuntober <ehuntercrypto@gmail.com>
2024-04-19 13:23:18 +0100

    new resource added

--- routes/forumRoutes.js
+++ routes/forumRoutes.js
@@ -6,6 +6,7 @@ const { verifyAPIKey, verifyToken, authorizeRole } = require('../utils/middlewar
 const router = express.Router();
 
 router.get('/', forumController.getAllForums);
+router.get('/:id',forumController.getForum)
 router.post('/', verifyAPIKey, verifyToken, authorizeRole('moderator'), forumController.createForum);
 router.put('/:id', verifyAPIKey, verifyToken, authorizeRole('moderator'), forumController.updateForum);
 router.delete('/:id', verifyAPIKey, verifyToken, authorizeRole('moderator'), forumController.deleteForum);
